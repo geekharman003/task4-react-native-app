8ca74f16750372907b6c98fa178e1e19644b7ba7
geekharman003 <harmansadhra2003@gmail.com>
2026-07-29 12:26:12 +0530

    setup the app navigation using react navigation

--- App.tsx
+++ App.tsx
@@ -1,12 +1,12 @@
-import { Text } from "react-native";
 import { ApolloProvider } from "@apollo/client/react";
 import React from "react";
 import { client } from "./src/apollo/client";
+import AppNavigator from "./src/navigation/AppNavigator";
 
 export default function App() {
   return (
     <ApolloProvider client={client}>
-      <Text>App</Text>
+      <AppNavigator></AppNavigator>
     </ApolloProvider>
   );
 }
